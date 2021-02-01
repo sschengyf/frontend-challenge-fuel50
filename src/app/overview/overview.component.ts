d306d921803026ae96b5b4ab80e7f6ea83732b4e
Ivan Cheng <sschengyf@gmail.com>
2021-02-01 22:13:33 +1300

    Add overview data

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,11 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Activity {
+  name: string;
+  figure: number;
+  diff: number;
+  upOrDown: 'up' | 'down';
+  variant: 'red' | 'green';
+}
+
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss'],
 })
 export class OverviewComponent implements OnInit {
+
+  readonly activities: Activity[] = [
+    {
+      name: 'Cat video watched',
+      figure: 1001,
+      diff: 80,
+      upOrDown: 'down',
+      variant: 'red'
+    },
+    {
+      name: 'Slices of pizza',
+      figure: 5,
+      diff: 2,
+      upOrDown: 'up',
+      variant: 'green'
+    },
+    {
+      name: 'Favorite songs beginning with the letter A',
+      figure: 1324,
+      diff: 23,
+      upOrDown: 'up',
+      variant: 'green'
+    }
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
--- src/app/overview/overview.module.ts
+++ src/app/overview/overview.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OverviewComponent } from './overview.component';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [OverviewComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
 })
 export class OverviewModule {}
