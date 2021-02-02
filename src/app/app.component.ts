9c7df5928954d7525adb6ce3cdf3cf7dcdaa0974
Ivan Cheng <sschengyf@gmail.com>
2021-02-02 22:32:37 +1300

    Make the navigation menu responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dashboard';
+  sidebarOpen = false;
+  showSidebar(show: boolean) {
+    this.sidebarOpen = show;
+  }
 }
